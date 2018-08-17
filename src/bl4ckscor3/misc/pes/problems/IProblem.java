bd5dc83030538359dc00062fd1751e0007d30635
bl4ckscor3 <donalddani9612@gmx.de>
2018-08-17 14:59:12 +0200

    add way of reading problem specific input file

--- src/bl4ckscor3/misc/pes/problems/IProblem.java
+++ src/bl4ckscor3/misc/pes/problems/IProblem.java
@@ -1,15 +1,40 @@
 package bl4ckscor3.misc.pes.problems;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
 public interface IProblem
 {
 	/**
 	 * Executes the problem
 	 */
 	public void exe();
-	
+
 	/**
 	 * Defines the name of the problem as written on the ProjectEuler page
 	 * @return The name of the problem
 	 */
 	public String name();
+
+	/**
+	 * @return A list containing all lines of the file for this problem ("problemXX.txt", where XX is the problem number).
+	 * 			May return an empty list, if an exception was thrown during the read process
+	 */
+	public default List<String> readFromProblemFile()
+	{
+		try
+		{
+			return FileUtils.readLines(new File("resources" + File.separator + getClass().getSimpleName().toLowerCase() + ".txt"), Charset.defaultCharset());
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			return new ArrayList<String>();
+		}
+	}
 }
--- src/bl4ckscor3/misc/pes/problems/Problem18.java
+++ src/bl4ckscor3/misc/pes/problems/Problem18.java
@@ -1,11 +1,6 @@
 package bl4ckscor3.misc.pes.problems;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.List;
-
-import org.apache.commons.io.FileUtils;
 
 public class Problem18 implements IProblem
 {
@@ -15,33 +10,26 @@ public class Problem18 implements IProblem
 	@Override
 	public void exe()
 	{
-		try
-		{
-			List<String> lines = FileUtils.readLines(new File("problem18.txt"), Charset.defaultCharset()); //read the triangle in the file
-			int[][] triangle = new int[lines.size()][]; //set up an array with the same amount of arrays as there are lines in the file
+		List<String> lines = readFromProblemFile(); //read the triangle in the file
+		int[][] triangle = new int[lines.size()][]; //set up an array with the same amount of arrays as there are lines in the file
 
-			for(int i = 0; i < lines.size(); i++)
+		for(int i = 0; i < lines.size(); i++)
+		{
+			String[] numbers = lines.get(i).split(" "); //put each number in its own array position
+
+			triangle[i] = new int[numbers.length]; //initialize the ith array
+
+			//populate the ith array
+			for(int j = 0; j < numbers.length; j++)
 			{
-				String[] numbers = lines.get(i).split(" "); //put each number in its own array position
-
-				triangle[i] = new int[numbers.length]; //initialize the ith array
-
-				//populate the ith array
-				for(int j = 0; j < numbers.length; j++)
-				{
-					triangle[i][j] = Integer.parseInt(numbers[j]);
-				}
+				triangle[i][j] = Integer.parseInt(numbers[j]);
 			}
+		}
 
-			sum = 0;
-			biggestSum = 0;
-			recursiveTriangle(triangle, 0, 0);
-			System.out.println("The maximum sum from top to bottom of the triangle in \"problem18.txt\" is " + biggestSum);
-		}
-		catch(IOException e) //in case FileUtils fails
-		{
-			e.printStackTrace();
-		}
+		sum = 0;
+		biggestSum = 0;
+		recursiveTriangle(triangle, 0, 0);
+		System.out.println("The maximum sum from top to bottom of the triangle in \"problem18.txt\" is " + biggestSum);
 	}
 
 	private void recursiveTriangle(int[][] triangle, int lineIndex, int numberIndex)
